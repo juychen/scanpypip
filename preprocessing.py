3a8c5207c0269f9d410a85ea823007a0bc6839c1
junyichen8 <junyichen8-c@my.cityu.edu.hk>
2019-07-12 15:53:51 -0400

    Edit mt

--- preprocessing.py
+++ preprocessing.py
@@ -4,16 +4,22 @@ def receipe_my(adata,l_n_genes = 500, r_n_genes= 5000, log = False,sparse = Fals
 
     sc.pp.filter_cells(adata, min_genes=200)
     sc.pp.filter_genes(adata, min_cells=3)
+    
+    mito_genes = adata.var_names.str.startswith('mt-')
 
     if sparse == False:    
         adata.obs['n_counts'] = adata.X.sum(axis=1)
+        adata.obs['percent_mito'] = np.sum(adata[:, mito_genes].X, axis=1) / np.sum(adata.X, axis=1)
     else:
         adata.obs['n_counts'] = adata.X.sum(axis=1).A1
+        adata.obs['percent_mito'] = np.sum(adata[:, mito_genes].X, axis=1).A1 / np.sum(adata.X, axis=1).A1
 
     adata = adata[
         np.logical_and(
         (adata.obs['n_genes'] > l_n_genes), 
         (adata.obs['n_genes'] < r_n_genes)),:]
+    adata = adata[adata.obs['percent_mito'] < 0.05, :]
+
 
     print(adata.shape)
     
