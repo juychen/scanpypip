a573b38a96b6e01b0418ebf30433e0be421dc181
junyichen8 <junyichen8-c@my.cityu.edu.hk>
2019-07-31 20:36:57 -0400

    Edit the nc ng function

--- preprocessing.py
+++ preprocessing.py
@@ -1,11 +1,8 @@
 import scanpy.api as sc
 import numpy as np
-from matplotlib import pyplot as plt
+from matplotlib import pyplot as plot
 
-def receipe_my(adata,l_n_genes = 500, r_n_genes= 5000, log = False,sparse = False,plotinfo= False):
-
-    sc.pp.filter_cells(adata, min_genes=200)
-    sc.pp.filter_genes(adata, min_cells=3)
+def cal_ncount_ngenes(adata,sparse=False):
     
     mito_genes = adata.var_names.str.startswith('mt-')
 
@@ -15,12 +12,29 @@ def receipe_my(adata,l_n_genes = 500, r_n_genes= 5000, log = False,sparse = Fals
     else:
         adata.obs['n_counts'] = adata.X.sum(axis=1).A1
         adata.obs['percent_mito'] = np.sum(adata[:, mito_genes].X, axis=1).A1 / np.sum(adata.X, axis=1).A1
+    return adata
+
+def receipe_my(adata,l_n_genes = 500, r_n_genes= 5000, percent_mito = 0.05, log = False,sparse = False,plotinfo= False):
+
+    sc.pp.filter_cells(adata, min_genes=200)
+    sc.pp.filter_genes(adata, min_cells=3)
+    
+    mito_genes = adata.var_names.str.startswith('mt-')
+
+    adata = cal_ncount_ngenes(adata)
+
+    # if sparse == False:    
+    #     adata.obs['n_counts'] = adata.X.sum(axis=1)
+    #     adata.obs['percent_mito'] = np.sum(adata[:, mito_genes].X, axis=1) / np.sum(adata.X, axis=1)
+    # else:
+    #     adata.obs['n_counts'] = adata.X.sum(axis=1).A1
+    #     adata.obs['percent_mito'] = np.sum(adata[:, mito_genes].X, axis=1).A1 / np.sum(adata.X, axis=1).A1
 
     adata = adata[
         np.logical_and(
         (adata.obs['n_genes'] > l_n_genes), 
         (adata.obs['n_genes'] < r_n_genes)),:]
-    adata = adata[adata.obs['percent_mito'] < 0.05, :]
+    adata = adata[adata.obs['percent_mito'] < percent_mito, :]
 
     if(plotinfo!=False):
         sc.pl.violin(adata, ['n_genes', 'n_counts', 'percent_mito'],
