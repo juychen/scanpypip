9a8a5bb34b2282681ece059db87fde71bdd463f8
Chen <che275@EEIAdmins-MBP.osumc.edu>
2019-08-07 10:14:31 -0400

    Edit combat

--- preprocessing.py
+++ preprocessing.py
@@ -2,12 +2,67 @@ import scanpy.api as sc
 import numpy as np
 from matplotlib import pyplot as plot
 
-def concat(adata_dict):
+def concat(adata_dict,join='inner', batch_key='sample', batch_categories=None, index_unique='-', combat = False, combat_key = "batch",covariates=None):
+    '''
+    This is a function to concat a dictonary of AnnData objects. Please predefine your own batch keys before the concatination
+
+    Params:
+    -------
+    adata_dict: `dict`, {"identity":AnnData from scanpy}
+        A dictionary: 
+        Values should AnnData objects: https://anndata.readthedocs.io/en/latest/api.html#module-anndata
+        Keys should be the identitiy of the adata object.
+    
+    join: `str`, optional (default: `"inner"`)
+        Use intersection (``'inner'``) or union (``'outer'``) of variables.
+    
+    batch_key: `str`, (default: `"sample"`)
+        Add the batch annotation to :attr:`obs` using this key.
+    
+    batch_categories: `str`,optional (default: `None`)
+        Use these as categories for the batch annotation. By default, use increasing numbers.
+    
+    index_unique, `str`, optional (default: `"-"`)
+        Make the index unique by joining the existing index names with the
+        batch category, using ``index_unique='-'``, for instance. Provide
+        ``None`` to keep existing indices.
+    
+    combat: `bool`, optional (defalut: `False`)
+        Decide if to to the batch effect correction
+    
+    combat_key: `str`, optional (default: `"batch"`)
+        Key to a categorical annotation from adata.obs that will be used for batch effect removal
+
+    covariates
+        Additional covariates such as adjustment variables or biological condition. Note that
+        not including covariates may introduce bias or lead to the removal of biological signal 
+        in unbalanced designs.
+
+    inplace: bool, optional (default: `True`)
+        Wether to replace adata.X or to return the corrected data
+
+    
+    Returns
+    -------
+    adata: AnnData
+        The concatenated AnnData, where "adata.obs[batch_key]"
+        stores a categorical variable labeling the original file identity.
+
+    '''
     ada_keys = list(adata_dict.keys())
 
     adata = adata_dict[ada_keys[0]].concatenate([adata_dict[k] for k in ada_keys[1:]]
-                                      ,batch_key="sample")
+                                      ,join=join
+                                      ,batch_key=batch_key
+                                      ,batch_categories=batch_categories
+                                      ,index_unique=index_unique)
     
+    if(combat == False):
+        return adata
+    
+    else:
+        sc.pp.combat(adata, key=combat_key, covariates=covariates)
+
     return adata
 
 def cal_ncount_ngenes(adata,sparse=False):
