8a55f4a66c446b2bd6cfeb6d8c83f1f417d42049
junyichen8 <junyichen8-c@my.cityu.edu.hk>
2019-08-15 18:54:42 -0400

    Add utils

---
+++ utils.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import scanpy as sc
+def get_de_dataframe(adata,index):
+    df_result = pd.DataFrame({
+            "names":[item[index] for item in adata.uns["rank_genes_groups"]["names"]]
+            ,"score":[item[index] for item in adata.uns["rank_genes_groups"]["scores"]]
+            , "pvals":[item[index] for item in adata.uns["rank_genes_groups"]["pvals"]]
+            , "pvals_adj":[item[index] for item in adata.uns["rank_genes_groups"]["pvals_adj"]]
+             })
+        
+    return df_result
